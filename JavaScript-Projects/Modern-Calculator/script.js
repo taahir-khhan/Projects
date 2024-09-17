ddfcc94d874b1c64f649da39591eb003bcae8acb
Tahir Khan <tahirtrk42@gmail.com>
2024-09-17 15:43:11 +0530

    fully functional modern calculator project complete

--- JavaScript-Projects/Modern-Calculator/script.js
+++ JavaScript-Projects/Modern-Calculator/script.js
@@ -6,7 +6,6 @@ class Calculator {
   }
 
   // Whenever first we load the application, the output screen should empty
-
   clear() {
     this.currentOperand = "";
     this.previousOperand = "";
@@ -14,27 +13,95 @@ class Calculator {
   }
 
   // Whenever we click delete button, it will remove the last digit of current Operand
-
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
   appendNumber(number) {
+    // if the currentOperand already contains a decimal point, then there is no sense to add more decimal points.
+    if (number === "." && this.currentOperand.includes(".")) return;
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const curr = parseFloat(this.currentOperand);
+
+    if (isNaN(prev) || isNaN(curr)) {
+      this.currentOperand = this.previousOperand;
+      this.previousOperand = "";
+      this.operation = "";
+      this.updateDisplay();
+      return;
+    }
+
+    switch (this.operation) {
+      case "+":
+        computation = prev + curr;
+        break;
+      case "-":
+        computation = prev - curr;
+        break;
+      case "÷":
+        computation = prev / curr;
+        break;
+      case "*":
+        computation = prev * curr;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+
+  // Try to as deep as possible to this functionality
   getDisplayNumber(number) {
-    return number.toString();
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
   }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.getDisplayNumber(
       this.currentOperand
     );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
+    }
   }
 }
-
-// Selecting all different elements on the page
 
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operand]");
@@ -53,6 +120,7 @@ const calculator = new Calculator(
   currentOperandTextElement
 );
 
+// Number buttons
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendNumber(button.innerText);
@@ -60,12 +128,28 @@ numberButtons.forEach((button) => {
   });
 });
 
+// Operation Buttons
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+// Delete Button
 deleteButton.addEventListener("click", () => {
   calculator.delete();
   calculator.updateDisplay();
 });
 
+// All clear Button
 allClearButton.addEventListener("click", () => {
   calculator.clear();
+  calculator.updateDisplay();
+});
+
+// Equals button
+equalButton.addEventListener("click", () => {
+  calculator.compute();
   calculator.updateDisplay();
 });
